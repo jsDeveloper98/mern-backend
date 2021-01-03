da8c90d8bba7aa1f4c06e1db08ccdaf2713f0305
jsDeveloper98 <Boyakhchyan1010@gmail.com>
2021-01-03 14:14:13 +0400

    add category to post

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const schema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   imgUrl: { type: String, required: true },
+  category: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   user: { type: Types.ObjectId, ref: "User" },
 });
